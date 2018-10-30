a95fc11924ed022af71c3b5f87a0a489aff4a380
Brendan Ko <brenny0707@gmail.com>
2018-10-30 15:57:21 -0400

    Add sample todo state

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,4 +1,19 @@
-const todosReducer = (state = {}, action) => {
+const sampleTodoState = {
+  1: {
+    id: 1,
+    title: 'wash car',
+    body: 'with soap',
+    done: false
+  },
+  2: {
+    id: 2,
+    title: 'wash dog',
+    body: 'with shampoo',
+    done: true
+  },
+}
+
+const todosReducer = (state = sampleTodoState, action) => {
   switch(action.type) {
     default:
       return state;
